148c49c0eadd7a1bf21771eb3b6212ae7b433c6c
coderkishornimje <kishornimaje1234@gmail.com>
2022-04-01 02:18:36 +0530

    link updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <h1>About</h1>
                 <p>This project was built for practice purposes using <strong>Context API</strong> by React.</p>
 
-                <a className="btn btn-primary" href="https://github.com/AlexSegen/react-shopping-cart">
+                <a className="btn btn-primary" href="https://github.com/coderkishornimje/shopping_cart">
                     <GitHubIcon width={"18px"}/> <span className="ml-2 mr-4">Visit Repo</span></a>
             </div>
         </Layout>
